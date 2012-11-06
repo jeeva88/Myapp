2e9cc9fa1f77b273ff98148f79237bda786e35bf
Jeeva Packiaraj <jeeva.packiaraj@gmail.com>
2012-11-05 22:19:42 -0500

    Getting that Sticker

---
+++ sticker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sticker::Application.config.secret_token = '013f27d73d05d1f5fe3249b31fa21cbeff3428d6294744d061fda8123e756bb59ce6a23f7c4e560b09efca9b7f4d4212a96780e7a7a54ad15f9dd42e597e4315'
